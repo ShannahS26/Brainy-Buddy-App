cc32dc82ba1510ed62eb56ad693ac9e22e9620df
Jie Zhou <Zhouj870@gmail.com>
2026-02-28 00:14:19 -0500

    Added login
    Still buggy and not registering correctly with password apparently too long. Need help debugging if anyone wants to look at it

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,6 +4,6 @@ from app.db.database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String, nullable=False)
+    username = Column(String(100), unique=True, nullable=False, index=True)
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(255), nullable=False)
